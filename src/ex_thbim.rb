121c879d53bd047b2c9b1012b0c05978fc97ca2b
THAPE\tiankaihe <1204973192@qq.com>
2022-11-30 13:20:08 +0800

    Version information modification

--- src/ex_thbim.rb
+++ src/ex_thbim.rb
@@ -8,11 +8,11 @@ module ThBM # TODO: Change module name to fit the project.
   module HelloCube
 
     unless file_loaded?(__FILE__)
-      ex = SketchupExtension.new('THAI Bim Extension', 'ex_thbim/main')
-      ex.description = 'THAI Bim Extension for SketchUp.'
+      ex = SketchupExtension.new('天华结构三维平台SU客户端', 'ex_thbim/main')
+      ex.description = 'SketchUp extension for TIANHUA Structure 3D Platform.'
       ex.version     = '1.0.0'
-      ex.copyright   = 'THAI © 2022-~'
-      ex.creator     = 'SketchUp'
+      ex.copyright   = 'Tianhua Architectural Design Co., Ltd'
+      ex.creator     = 'TIANHUA AI Research Center'
       Sketchup.register_extension(ex, true)
       file_loaded(__FILE__)
     end
